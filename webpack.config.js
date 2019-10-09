0d774fbeaffa500a43520e9f3904011a930f7d34
littlee <usedlittlee@gmail.com>
2019-10-09 12:39:59 +0800

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   output: {
     library: 'CanvasSprite',
     libraryExport: 'default',
